2724ecd8bfff21dc7d6139fea62bac685f02e0ca
sumith-tech <sumith2200.ss@gmail.com>
2023-06-19 12:51:29 +0530

    passing Data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import ExpenseItem from "./components/ExpenseItem";
-import Item from "./components/Items";
+
 function App() {
+  const expenses = [
+    {
+      item: "books",
+      price: 100,
+      date: new Date(2023, 5, 22),
+      location: "chennai",
+    },
+    {
+      item: "outfit",
+      price: 300,
+      date: new Date(2023, 5, 16),
+      location: "t-nagar",
+    },
+    {
+      item: "bat",
+      price: 180,
+      date: new Date(2023, 3, 2),
+      location: "madvaram",
+    },
+  ];
   return (
     <div>
       <h2>Lesson 1</h2>
-      <ExpenseItem></ExpenseItem>
-      <Item></Item>
+      <ExpenseItem
+        title={expenses[0].item}
+        amount={expenses[0].price}
+        date={expenses[0].date}
+        location={expenses[0].location}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].item}
+        amount={expenses[1].price}
+        date={expenses[1].date}
+        location={expenses[1].location}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].item}
+        amount={expenses[2].price}
+        date={expenses[2].date}
+        location={expenses[2].location}
+      ></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,16 @@
-const ExpenseItem = () => <h2>Expense item!</h2>;
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div>{props.location}</div>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
+};
 
 export default ExpenseItem;
